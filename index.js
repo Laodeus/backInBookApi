636fdfffeeece336d5a9dca52e72318b76011c1d
Jonathan <atta.jon.t@gmail.com>
2019-10-03 20:28:49 +0200

    assert Test... error handle... it's awesome again

--- index.js
+++ index.js
@@ -2,18 +2,15 @@ let Koa = require("koa");
 let app = new Koa();
 
 app.use(async (ctx, next) =>{ 
-        ctx.body = "Hello koa world";
-        await next();
+        ctx.body = "Hello koa world"; // this is how we send a reponse
+        await next(); // everyting that's under this in this bloc will be processed after the next .use
         console.log("after await")
     }
 )
 .use((ctx, next)=>{
-        console.log("in the next");
-        next();
-    }
-)
-.use((ctx, next)=>{
-        console.log("another next")
+        console.log("another next");
+        console.log(ctx)
+        ctx.assert(false, 406); // 406 mean inacceptable. so assert take a bool in params and the code of response if this params is false.
     }
 )
 .use((ctx, next)=>{
