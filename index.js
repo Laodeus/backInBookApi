5dc6b0848c57cb659a0ff9e64f9d918d9c7d5855
Jonathan <atta.jon.t@gmail.com>
2019-10-03 20:18:45 +0200

    Next() test... It's awesome how it's working!

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 let Koa = require("koa");
 let app = new Koa();
 
-app.use(async (ctx, next) =>{
+app.use(async (ctx, next) =>{ 
         ctx.body = "Hello koa world";
+        await next();
+        console.log("after await")
     }
 )
-
+.use((ctx, next)=>{
+        console.log("in the next");
+        next();
+    }
+)
+.use((ctx, next)=>{
+        console.log("another next")
+    }
+)
+.use((ctx, next)=>{
+    console.log("No next")
+    }
+)
 .listen(3000,()=>{
     console.clear();
     console.log(`Client Initialisation`);
