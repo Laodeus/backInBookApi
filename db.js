0267088a4ede762f5f9c617e44084ce5ce960f77
dylanevelette <dylan.e2802gmail.com>
2019-10-04 10:07:24 +0200

    Test koa

---
+++ db.js
@@ -0,0 +1,33 @@
+const books = [
+    {
+        titre: 'Elon Musk',
+        soustitre: 'Tesla,SpaceX,and the guest for a fantastic future',
+        auteur: 'Ashlee Vance',
+        editeur: 'Elon musk',
+        format: 'physique',
+        langue: 'english',
+        couverture: 'couverture',
+        stock: 1
+    },
+    {
+        titre: 'f',
+        soustitre: 'f',
+        auteur: 'f',
+        editeur: 'editeur',
+        format: 'physique',
+        langue: 'english',
+        couverture: '',
+        stock: 2
+    },
+    {
+        titre: 'Jonathan',
+        soustitre: 'je vous explique ma premiere fois',
+        auteur: 'Ashlee Vance',
+        editeur: 'Elon musk',
+        format: 'physique',
+        langue: 'english',
+        couverture: 'couverture',
+        stock: 10
+    }
+
+];
