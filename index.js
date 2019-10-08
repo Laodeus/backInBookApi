de972040d05e59dc8ef838af3911c35c19341555
Jonathan <atta.jon.t@gmail.com>
2019-10-08 14:51:54 +0200

    Port env and npm start done for depoyelement

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+//library import
+
 const Koa = require('koa');
 const mount = require('koa-mount');
 const graphqlHTTP = require('koa-graphql');
+
+//personal import
 const mySchema = require("./schema/schema"); 
+
+//library init
 const app = new Koa();
- 
+
+//setting env
+const port = process.env.PORT || 3000
+
 app.use(mount('/graphql', graphqlHTTP({
   schema: mySchema,
   graphiql: true
 })));
  
-app.listen(3615,()=>{
+app.listen(port,()=>{
   console.clear();
-  console.log("The magic start Below!");
+  console.log("The magic start below at port : " + port);
 });
