3f8316b79339702878e14e5745c82e4d0d28090b
Jonathan <atta.jon.t@gmail.com>
2019-10-08 13:23:53 +0200

    recusivity test

--- schema/schema.js
+++ schema/schema.js
@@ -74,6 +74,12 @@ const RootQuery = new GraphQLObjectType({
         resolve(parent, args){
             return _.find(authors, { id: args.id });
         }
+    },
+    books:{
+        type: new GraphQLList(BookType),
+        resolve(parent,args){
+            return books
+        }
     }
   }
 });
